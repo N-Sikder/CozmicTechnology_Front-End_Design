7e3557ade7ed0d86e860a200cdf54c8a70894f53
N-Sikder <velocityx72@gmail.com>
2024-04-04 16:31:30 +0600

    Carousel added to the index page and styled.

---
+++ script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('.hero-wrapper').slick({
+        dots: true,
+        autoplay: true,
+        autoplaySpeed:5000,
+        arrows: true,
+        prevArrow: '<i class="fas fa-arrow-circle-left slick-prev"></i>',
+        nextArrow: '<i class="fas fa-arrow-circle-right slick-next"></i>',
+    });
+    
+  });
